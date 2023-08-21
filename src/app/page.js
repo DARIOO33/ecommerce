840608278fffbb5f9d4bb4397b00cc7c6ff1d480
DARIOO33 <darioanwar3@gmail.com>
2023-08-21 18:09:09 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
 import Image from 'next/image'
-
-export default function Home() {
+import Link from 'next/link'
+import SellCard from "./components/SellCard/SellCard"
+// import Pagenavigation from "./components/Pagenavigation/Pagenavigation"
+import "./globals.css"
+async function getProducts() {
+  let res = await fetch("http://localhost:3000/api/products")
+  return res.json()
+}
+export default async function Home() {
+  let products = await getProducts()
+  // console.log(typeof (products));
+  const currentPosts = products.slice(0, 4)
   return (
-    <h1 className='text-center text-5xl font-extrabold'>
-      Test
-    </h1>
+    <div>
+
+
+      {currentPosts.map((product => (
+        <SellCard key={product.id} price={product.price} productName={product.name} description={product.description} ratings={Math.floor(Math.random() * 5)} />
+      )))}
+      {/* <Pagenavigation one="0" two="1" three="2" /> */}
+    </div>
   )
 }
