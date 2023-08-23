48231cc8de240c7db4fa071a0a8802f838356283
DARIOO33 <darioanwar3@gmail.com>
2023-08-23 09:03:10 +0100

    fixed page navigation

--- src/app/components/navigation/Navigation.jsx
+++ src/app/components/navigation/Navigation.jsx
@@ -4,50 +4,53 @@ import { usePathname } from 'next/navigation'
 import './n.css'
 export default function Navigation(params) {
     const pathname = usePathname()
+    const path = pathname.slice(1);
+    const slash = path.indexOf('/')
+    const pageIndex = path.substr(slash + 1, path.length - slash)
+
     function prevpage() {
         if (pathname.length == 1) {
             return 1
         }
-        const path = pathname.slice(1);
-        const slash = path.indexOf('/')
-        // console.log(slash);
-        const pageIndex = path.substr(slash + 1, path.length - slash)
-        // console.log(pageIndex);
         let prev = Number(pageIndex) - 1
         if (prev < 1) {
             prev = 1
         }
         return prev.toString()
     }
-    function nextage() {
+    function nextpage() {
         console.log(pathname);
         if (pathname.length == 1) {
             return 2
         }
-
-        const path = pathname.slice(1);
-        const slash = path.indexOf('/')
-        // console.log(slash);
-        const pageIndex = path.substr(slash + 1, path.length - slash)
-        // console.log(pageIndex);
         const prev = Number(pageIndex) + 1
         return prev.toString()
     }
     let prevv, nextt;
+    const pageKeyword = "page/";
     if (pathname.includes("page")) {
         prevv = prevpage()
-        nextt = nextage()
+        nextt = nextpage()
     }
     else {
-        prevv = "page/" + prevpage()
-        nextt = "page/" + nextage()
-
-
+        prevv = pageKeyword + prevpage()
+        nextt = pageKeyword + nextpage()
     }
-    const prevlink = prevv
     const nextlink = nextt
+    const prevlink = prevv
     const prev = "<<"
     const next = ">>"
+    let first, second, third;
+    if (pageIndex == 1) {
+        first = pageIndex
+        second = Number(pageIndex) + 1
+        third = Number(pageIndex) + 2
+    }
+    else {
+        first = Number(pageIndex) - 1
+        second = Number(pageIndex)
+        third = Number(pageIndex) + 1
+    }
     return (
         <div className="m-auto justify-between  text-center mt-12 flex items-center w-64 h-12 ">
             <Link href={prevlink}>
@@ -56,10 +59,16 @@ export default function Navigation(params) {
                 </div>
             </Link>
             <div className="itemm px-2">
-                1
+                <Link href={prevlink}>
+                    {first}
+                </Link>
             </div>
-            <div className="itemm px-2">2</div>
-            <div className="itemm px-2">3</div>
+            <div className="itemm px-2">{second}</div>
+            <div className="itemm px-2">
+                <Link href={nextlink}>
+                    {third}
+                </Link>
+            </div>
             <Link href={nextlink}>
                 <div className="itemm px-2 next">
 
