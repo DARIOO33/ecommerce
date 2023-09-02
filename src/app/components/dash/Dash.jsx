c4f37eeb020367b80a494e8fd3d78d998fb34f49
DARIOO33 <darioanwar3@gmail.com>
2023-09-02 12:22:59 +0100

    some responsive updates

--- src/app/components/dash/Dash.jsx
+++ src/app/components/dash/Dash.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { AiFillFunnelPlot } from "react-icons/ai";
 import './dashboard.css'
 import PriceParams from './PriceParams.jsx'
 import Categories from './Categories.jsx'
@@ -17,14 +17,16 @@ export default function Dash() {
 
     return (
         <>
-            <p className='filtre mt-36 cursor-pointer laptop:hidden' onClick={handleclick}>Click Me!</p>
+            <div className=' flex justify-center items-center h-14 filtre mt-36 cursor-pointer laptop:hidden ' onClick={handleclick}><AiFillFunnelPlot /> <span>Filtre</span> </div>
 
             <div className={activedash ? "activedash dash w-1/3" : "dash w-1/3"}>
                 <div className="dashboard rounded-xl ">
+                    <p onClick={handleclick} className="close font-sans text-4xl float-right relative bottom-8 mr-4 cursor-pointer text-white">X</p>
+                    <br />
+
                     <h1 className='text-2xl pt-4 text-center font-bold'>Price</h1>
                     <PriceParams />
                     <Categories />
-
                 </div>
             </div>
         </ >
